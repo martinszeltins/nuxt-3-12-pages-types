0d384c3ad9b93938674dcd42baa8f79b16e882bb
Martins Zeltins <queeeeenz@gmail.com>
2024-06-14 17:10:39 +0300

    Initial commit

---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>Index Page</div>
+
+    <!-- "to" autocomplete broken here -->
+    <NuxtLink to="/user-1">User Page</NuxtLink>
+</template>
+
+<script setup lang="ts">
+    definePageMeta({ name: 'index' })
+
+    const route = useRoute() // Autocomplete broken here 
+</script>
---
+++ pages/user-[id].vue
@@ -0,0 +1,11 @@
+<template>
+    <!-- $route is of type "any" -->
+    <div>User Page, id: {{ $route.params.id }}</div>
+
+    <!-- "to" autocomplete broken here -->
+    <NuxtLink to="/">Index Page</NuxtLink>
+</template>
+
+<script setup lang="ts">
+    definePageMeta({ name: 'user' })
+</script>
